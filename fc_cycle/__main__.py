38f8e7b7ba9b2a9ce40d1be6d2b041ee0c82df3e
Nader Morshed <morshed.nader@gmail.com>
2017-04-17 23:22:55 -0400

    Use serial LineReader
    Added argparser

--- fc_cycle/__main__.py
+++ fc_cycle/__main__.py
@@ -1,4 +1,6 @@
 
+from __future__ import absolute_import
+
 import sys
 
 from fc_cycle import main
--- fc_cycle/main.py
+++ fc_cycle/main.py
@@ -1,48 +1,40 @@
 
+from __future__ import absolute_import
+
+import argparse
 import logging
 import math
 import serial
+import serial.threaded
 import sys
 import time
+import traceback
+
+from fc_cycle import __version__
 
 LOGGER = logging.getLogger("fc_cycle.main")
 
 
-class SerialWrapper(object):
-    def __init__(self, f, encode_bytes=True):
-        self.f = f
-        self.encode_bytes = encode_bytes
+class HandleSerial(serial.threaded.LineReader):
+    def connection_made(self, transport):
+        super(HandleSerial, self).connection_made(transport)
+        LOGGER.info('port opened')
 
-    def write(self, b):
-        LOGGER.debug("Write:", b)
+    def handle_packet(self, data):
+        LOGGER.debug("received packet: {}".format(repr(data)))
+        super(HandleSerial, self).handle_packet(data)
 
-        if not isinstance(b, bytes) and self.encode_bytes:
-            b = b.encode()
+    def handle_line(self, data):
+        LOGGER.info("received line: {}".format(repr(data)))
 
-        return self.f.write(b)
+    def write_line(self, text):
+        LOGGER.info("writing line: {}".format(repr(text)))
+        super(HandleSerial, self).write_line(text)
 
-    def writeline(self, b):
-        if not isinstance(b, bytes) and self.encode_bytes:
-            b = b.encode() + b"\n"
-        else:
-            b = b + "\n"
-
-        return self.write(b)
-
-    def read(self):
-        b = self.f.read()
-        LOGGER.debug("Read:", b)
-        return b
-
-    def close(self):
-        return self.f.close()
-
-    def __enter__(self):
-        self.f.__enter__()
-        return self
-
-    def __exit__(self, *args, **kwargs):
-        return self.f.__exit__(*args, **kwargs)
+    def connection_lost(self, exc):
+        if exc:
+            traceback.print_exc(exc)
+        LOGGER.info('port closed')
 
 
 def _get_serial(serial_name=None):
@@ -59,7 +51,12 @@ def _get_serial(serial_name=None):
         "COM4",
     ]:
         try:
-            port = serial.Serial(path)
+            port = serial.Serial(
+                path,
+                baudrate=19200,  # 9600?
+                parity=serial.PARITY_EVEN,  # PARITY_NONE?
+                stopbits=serial.STOPBITS_ONE,
+            )
         except serial.serialutil.SerialException as e:
             excepts.append(e)
             continue
@@ -69,7 +66,79 @@ def _get_serial(serial_name=None):
     raise Exception("Unable to find any valid serial ports", excepts)
 
 
+def _parse_args(args):
+    """
+    Parses arguments from a argv format.
+
+    Parameters
+    ----------
+    args : list of str
+
+    Returns
+    -------
+    argparse.ArgumentParser
+    """
+    parser = argparse.ArgumentParser(
+        prog="FC-Cycle",
+        description="Utility for collecting fraction in a cycle on the Gilson "
+        "FC 204 Fraction Collector.",
+    )
+    parser.add_argument(
+        "-v", "--verbose",
+        action="count",
+        help="Increase verbosity of output.",
+    )
+    parser.add_argument(
+        "-q", "--quiet",
+        action="count",
+        help="Decrease verbosity of output.",
+    )
+    parser.add_argument(
+        '-V', '--version',
+        action="version",
+        version="%(prog)s {}".format(__version__),
+    )
+    return parser, parser.parse_args(args)
+
+
+def _set_verbosity(args):
+    verbosity = (args.verbose or 0) - (args.quiet or 0)
+
+    if verbosity <= -2:
+        level = logging.CRITICAL
+    elif verbosity == -2:
+        level = logging.ERROR
+    elif verbosity == -1:
+        level = logging.WARNING
+    elif verbosity == 0:
+        level = logging.INFO
+    elif verbosity > 0:
+        level = logging.DEBUG
+
+    logger = logging.getLogger('fc_cycle')
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(level)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    fh = logging.FileHandler('fc_cycle.log')
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    LOGGER.debug(sys.argv)
+    LOGGER.debug(args)
+
+
 def main(args):
+    _, args = _parse_args(args)
+    _set_verbosity(args)
+
     serial_name = input("Serial Port Name (optional): ")
     max_tubes = int(input("Max Tubes (default: 20): ") or 20)
     delay = int(input("Delay in Minutes (default: 5): ") or 5)
@@ -79,24 +148,31 @@ def main(args):
     total_time *= 60
     delay *= 60
 
-    with SerialWrapper(_get_serial(serial_name)) as ser:
-        # Beep and display Hello World
-        ser.writeline("G010")
-        ser.writeline("W0Hello")
-        ser.writeline("W01World")
-        ser.writeline("V1")
-        time.sleep(delay)
-        ser.writeline("V0")
+    with serial.ReaderThread(
+        _get_serial(serial_name),
+        HandleSerial,
+    ) as ser:
+        try:
+            # Beep and display Hello World
+            ser.write_line("G010")
+            ser.write_line("W0Hello")
+            ser.write_line("W01World")
+            ser.write_line("V1")
+            time.sleep(delay)
+            ser.write_line("V0")
 
-        # Cycle through each tube
-        for i in range(math.ceil((total_time - delay) / tube_time)):
-            ser.writeline("T{:03d}".format((i % max_tubes) + 1))
-            time.sleep(tube_time)
-
-        # Reset the machine
-        ser.writeline("V1")
-        time.sleep(10)
-        ser.writeline("$")
+            # Cycle through each tube
+            for i in range(math.ceil((total_time - delay) / tube_time)):
+                ser.write_line("T{:03d}".format((i % max_tubes) + 1))
+                time.sleep(tube_time)
+        except KeyboardInterrupt as err:
+            LOGGER.info("Closing connection")
+        finally:
+            # Reset the machine
+            ser.write_line("V1")
+            time.sleep(10)
+            ser.write_line("$")
+            ser.close()
 
 
 if __name__ == "__main__":
